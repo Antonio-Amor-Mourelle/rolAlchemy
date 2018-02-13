d09de4faed056cef033b3d57517fa8f44dc0f82c
Antonio Amor <antonio.amor.mourelle@gmail.com>
2018-02-13 01:02:02 +0100

    populate working with new interface (list inside __inits__)

--- populate.py
+++ populate.py
@@ -4,9 +4,9 @@
 from base import Session, engine, Base
 from material import Material
 from recipe import Recipe
-from bagMaterials import BagMaterials
-from bagRecipes import BagRecipes
 from alchemist import Alchemist
+from bagMaterials import BagMaterials
+from bagPotions import BagPotions
 
 from sqlalchemy.exc import IntegrityError
 
@@ -29,39 +29,44 @@ session.commit()
 print('material commit ok')
 
 #5 - creates recipes
-r1 = Recipe("receta 1", "Esta es la receta 1")
-r2 = Recipe("receta 2", "Esta es la receta 1")
-r3 = Recipe("receta 3", "Esta es la receta 1")
+r1 = Recipe("receta 1", "Esta es la receta 1",[azufre])
+r2 = Recipe("receta 2", "Esta es la receta 2",[sal])
+r3 = Recipe("receta 3", "Esta es la receta 3",[sal,sal, acido])
 
-r1.materials = [azufre]
-r2.materials = [sal]
-r3.materials = [sal,sal, acido]
+
+session.add(r1)
+session.add(r2)
+session.add(r3)
 
 session.commit()
 print('recipes commit ok')
 
 
 # ** - add alchemists
-al1= Alchemist('ton')
-al1.recipes=[r1,r3]
+al1= Alchemist('ton',list(set([r1,r3])))
+al2= Alchemist('javi',list(set([r2,r3])))
 
 session.add(al1)
+session.add(al2)
 
 session.commit()
 print('alchemist commit ok')
 
-'''
-# 7 - add materials to bag
-bm1 = BagMaterials(azufre.id, 2)
-# 8 - add recipes to bag
-br1 = BagRecipes(r1.id, 1)
-br2 = BagRecipes(r2.id, 3)
+bm1=BagMaterials(1,1,5)
+bm2=BagMaterials(2,3,10)
 
 session.add(bm1)
+session.add(bm2)
 
-session.add(br1)
-session.add(br2)
+
+bp1=BagPotions(1,2,2)
+bp2=BagPotions(2,1,5)
+
+session.add(bp2)
+session.add(bp2)
 
 session.commit()
-'''
+
+print('bag commit ok')
+
 session.close()
