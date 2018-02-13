0370d66adba3678e6f059a409f02bbff3e084d80
Antonio Amor <antonio.amor.mourelle@gmail.com>
2018-02-13 01:00:37 +0100

    recipes added in __init

--- alchemist.py
+++ alchemist.py
@@ -13,9 +13,11 @@ alchemist_recipes_association = Table('alchemists_recipes', Base.metadata,
 
 class Alchemist(Base):
     __tablename__ = 'alchemists'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     recipes = relationship("Recipe", secondary=alchemist_recipes_association)
 
-    def __init__(self, name):
+    def __init__(self, name, l=[]):
         self.name = name
+        self.recipes=l
